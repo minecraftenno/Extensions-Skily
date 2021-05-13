19332b4d18477120be51231ef3737517a3ed44cb
minecraftenno <68814721+minecraftenno@users.noreply.github.com>
2021-05-13 19:45:50 +0200

    Ajout de l'extension "Minecraft Server Status"

---
+++ minecraft-status/mcstat.bat
@@ -0,0 +1,10 @@
+@echo off
+title Skily - Minecraft Server Statut
+color b
+echo Demarrage du bot...
+node mcstat.js
+echo Nous avons détécter que le bot est éteint, si vous voulez le redemarrer appuyez sur une touche. Si vous voulez l'éteindre complètement fermez cette fenêtre.
+pause
+node mcstat.js
+echo Êteinte du bot....
+pause
